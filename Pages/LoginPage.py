aa774dda4d38bbe876bf70a5ea3c6aecef9562b2
Puneeth456 <shivanpuneeth50@gmail.com>
2020-05-18 13:32:36 +0530

    Initial commit

---
+++ Pages/LoginPage.py
@@ -0,0 +1,24 @@
+from Pages.Webgeneric import Webgeneric
+from Data.Testdata import *
+
+class Login(Webgeneric):
+    def __init__(self,driver):
+        Webgeneric.__init__(self,driver)
+        self.un="email"
+        self.pwd="pass"
+        self.login= "//input[@type='submit']"
+        global wb
+        wb=Webgeneric(driver)
+
+
+    def enter_un(self):
+        wb.enter("id",self.un,Username)
+
+
+    def enter_pwd(self):
+        wb.enter("pwd",self.pwd,Password)
+
+
+    def click_login(self):
+        wb.click("click",self.login)
+
---
+++ Tests/test_login.py
@@ -0,0 +1,12 @@
+from Pages.LoginPage import Login
+import pytest
+from Data.Testdata import *
+
+@pytest.mark.usefixtures("test_setup")
+class Testlogin():
+    def test_fblogin(self):
+        driver=self.driver
+        lg=Login(driver)
+        lg.enter_un()
+        lg.enter_pwd()
+        lg.click()
